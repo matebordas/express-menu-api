bdd549d38291ad89cb5488e6f1a809ae025a6a9c
Mate Balo Bordas <mate.balob@gmail.com>
2023-08-13 19:12:43 +0100

    wip - add the first DAO function

---
+++ src/menuItems/menuItems.dao.ts
@@ -0,0 +1,22 @@
+import {dbPool} from "../db/dbConnection";
+import {MenuItem} from "./types";
+import {RowDataPacket} from "mysql2/typings/mysql/lib/protocol/packets/RowDataPacket";
+
+interface MenuItemRecord extends MenuItem, RowDataPacket {}
+
+async function selectAll(): Promise<MenuItem[]> {
+  const query = 'SELECT * from menu_items';
+
+  return new Promise((resolve, reject)=> {
+    dbPool.query<MenuItemRecord[]>(query, function (error, results) {
+      if (error) {
+        reject(error)
+      }
+      resolve(results);
+    });
+  });
+}
+
+export {
+  selectAll
+}
