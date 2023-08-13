fb624f5ae36ded7d8c7ed3c97f037eaae8ab85a9
Mate Balo Bordas <mate.balob@gmail.com>
2023-08-13 19:01:02 +0100

    small refactor on the menuItems router

--- src/menuItems/menuItems.router.ts
+++ src/menuItems/menuItems.router.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import * as ItemController from "./menuItems.controller";
 
-export const menuItemsRouter = express.Router();
+const menuItemsRouter = express.Router();
 
 const menuItemsRouteBase = '/menu/items';
 
@@ -14,3 +14,7 @@ menuItemsRouter.post(`${menuItemsRouteBase}/`, ItemController.createMenuItem);
 menuItemsRouter.put(`${menuItemsRouteBase}/:id`, ItemController.updateMenuItem);
 
 menuItemsRouter.delete(`${menuItemsRouteBase}/:id`, ItemController.deleteMenuItem);
+
+export {
+  menuItemsRouter
+}
