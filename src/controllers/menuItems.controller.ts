016b575e467e931812123b8b8f528ce48c284520
Mate Balo Bordas <mate.balob@gmail.com>
2023-08-18 22:55:31 +0100

    make MenuItemsController injectable

--- src/controllers/menuItems.controller.ts
+++ src/controllers/menuItems.controller.ts
@@ -1,89 +1,115 @@
+import { Service } from "typedi";
 import { Request, Response } from "express";
-import * as ItemService from "../services/menuItems.service";
 
 import { MenuItem, BaseMenuItem } from "../types";
+import { MenuItemsService } from "../services/MenuItems.service";
 
-const defaultErrorMessage = "An error occurred";
+const DEFAULT_ERROR_MESSAGE = "An error occurred";
 
 function getErrorMessage(error: unknown) {
   if (error instanceof Error) {
     return error.message;
   }
 
-  return defaultErrorMessage;
+  return DEFAULT_ERROR_MESSAGE;
 }
 
-export async function getMenuItems(req: Request, res: Response) {
-  try {
-    const items: MenuItem[] = await ItemService.findAll();
-
-    res.status(200).send(items);
-  } catch (e) {
-    const errorMessage = getErrorMessage(e);
-    res.status(500).send(errorMessage);
+function logError(error: unknown) {
+  if (error instanceof Error) {
+    console.log(error.stack);
   }
+
+  console.log(DEFAULT_ERROR_MESSAGE);
 }
 
-export async function getMenuItemById(req: Request, res: Response) {
-  const id: number = parseInt(req.params.id, 10);
+@Service()
+export class MenuItemsController {
+  private menuItemsService: MenuItemsService;
 
-  try {
-    const item: MenuItem = await ItemService.find(id);
+  constructor(menuItemsService: MenuItemsService) {
+    this.menuItemsService = menuItemsService;
+    this.findAll.bind(this);
+  }
 
-    if (item) {
-      return res.status(200).send(item);
+  async findAll(req: Request, res: Response) {
+    try {
+      const items: MenuItem[] = await this.menuItemsService.findAll();
+
+      res.status(200).send(items);
+    } catch (e) {
+      logError(e);
+      const errorMessage = getErrorMessage(e);
+      res.status(500).send(errorMessage);
     }
-
-    res.status(404).send("item not found");
-  } catch (e: unknown) {
-    const errorMessage = getErrorMessage(e);
-    res.status(500).send(errorMessage);
   }
-}
 
-export async function createMenuItem(req: Request, res: Response) {
-  try {
-    const item: BaseMenuItem = req.body;
-
-    const newItem = await ItemService.create(item);
-
-    res.status(201).json(newItem);
-  } catch (e: unknown) {
-    const errorMessage = getErrorMessage(e);
-    res.status(500).send(errorMessage);
-  }
-}
-
-export async function updateMenuItem(req: Request, res: Response) {
-  const id: number = parseInt(req.params.id, 10);
-
-  try {
-    const itemUpdate: MenuItem = req.body;
-
-    const existingItem: MenuItem = await ItemService.find(id);
-
-    if (existingItem) {
-      const updatedItem = await ItemService.update(id, itemUpdate);
-      return res.status(200).json(updatedItem);
-    }
-
-    const newItem = await ItemService.create(itemUpdate);
-
-    res.status(201).json(newItem);
-  } catch (e: unknown) {
-    const errorMessage = getErrorMessage(e);
-    res.status(500).send(errorMessage);
-  }
-}
-
-export async function deleteMenuItem(req: Request, res: Response) {
-  try {
+  async find(req: Request, res: Response) {
     const id: number = parseInt(req.params.id, 10);
-    await ItemService.remove(id);
 
-    res.sendStatus(204);
-  } catch (e: unknown) {
-    const errorMessage = getErrorMessage(e);
-    res.status(500).send(errorMessage);
+    try {
+      const item: MenuItem = await this.menuItemsService.find(id);
+
+      if (item) {
+        res.status(200).send(item);
+        return;
+      }
+
+      res.status(404).send("item not found");
+    } catch (e: unknown) {
+      logError(e);
+      const errorMessage = getErrorMessage(e);
+      res.status(500).send(errorMessage);
+    }
+  }
+
+  async create(req: Request, res: Response) {
+    try {
+      const item: BaseMenuItem = req.body;
+
+      const newItem = await this.menuItemsService.create(item);
+
+      res.status(201).json(newItem);
+    } catch (e: unknown) {
+      logError(e);
+      const errorMessage = getErrorMessage(e);
+      res.status(500).send(errorMessage);
+    }
+  }
+
+  async update(req: Request, res: Response) {
+    const id: number = parseInt(req.params.id, 10);
+
+    try {
+      const itemUpdate: MenuItem = req.body;
+
+      const existingItem: MenuItem = await this.menuItemsService.find(id);
+
+      if (existingItem) {
+        const updatedItem = await this.menuItemsService.update(id, itemUpdate);
+        res.status(200).json(updatedItem);
+        return;
+      }
+
+      const newItem = await this.menuItemsService.create(itemUpdate);
+
+      res.status(201).json(newItem);
+    } catch (e: unknown) {
+      logError(e);
+      const errorMessage = getErrorMessage(e);
+      res.status(500).send(errorMessage);
+    }
+  }
+
+  async delete(req: Request, res: Response) {
+    try {
+      const id: number = parseInt(req.params.id, 10);
+      await this.menuItemsService.delete(id);
+
+      res.sendStatus(204);
+    } catch (e: unknown) {
+      logError(e);
+      const errorMessage = getErrorMessage(e);
+      res.status(500).send(errorMessage);
+    }
   }
 }
