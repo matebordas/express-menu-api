6ba34a48e49887dc8b7b0fc0faf6138bfcf49563
Mate Balo Bordas <mate.balob@gmail.com>
2023-08-18 18:08:10 +0100

    make the MenuItemsRepository an injectable class

--- src/repositories/menuItems.repository.ts
+++ src/repositories/menuItems.repository.ts
@@ -1,20 +1,23 @@
+import { Service } from "typedi";
+
 import { dbPool } from "../config/db.config";
 import { MenuItem } from "../types";
 import { RowDataPacket } from "mysql2/typings/mysql/lib/protocol/packets/RowDataPacket";
 
 interface MenuItemRecord extends MenuItem, RowDataPacket {}
 
-async function selectAll(): Promise<MenuItem[]> {
-  const query = "SELECT * from menu_items";
+@Service()
+export class MenuItemsRepository {
+  async findAll(): Promise<MenuItem[]> {
+    const query = "SELECT * from menu_items";
 
-  return new Promise((resolve, reject) => {
-    dbPool.query<MenuItemRecord[]>(query, function (error, results) {
-      if (error) {
-        reject(error);
-      }
-      resolve(results);
+    return new Promise((resolve, reject) => {
+      dbPool.query<MenuItemRecord[]>(query, function (error, results) {
+        if (error) {
+          reject(error);
+        }
+        resolve(results);
+      });
     });
-  });
+  }
 }
-
-export { selectAll };
